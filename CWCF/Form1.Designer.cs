df450aa21f98b6a6f87571a196654c464c29cbc3
RRRiderrr <77801822+RRRiderrr@users.noreply.github.com>
2024-04-19 18:03:57 +0300

    Update v1.1

--- CWCF/Form1.Designer.cs
+++ CWCF/Form1.Designer.cs
@@ -38,12 +38,14 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(172, 428);
+            this.button1.Location = new System.Drawing.Point(64, 422);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 37);
@@ -55,7 +57,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(148, 104);
+            this.textBox1.Location = new System.Drawing.Point(148, 117);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.MaxLength = 3;
             this.textBox1.Name = "textBox1";
@@ -173,7 +175,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 107);
+            this.label1.Location = new System.Drawing.Point(18, 120);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 21);
@@ -194,11 +196,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(297, 475);
+            this.label3.Location = new System.Drawing.Point(308, 483);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(144, 21);
             this.label3.TabIndex = 5;
-            this.label3.Text = "Made by Rider v1.0";
+            this.label3.Text = "Made by Rider v1.1";
             // 
             // label4
             // 
@@ -224,17 +226,38 @@
             // 
             this.label6.AutoSize = true;
             this.label6.ForeColor = System.Drawing.Color.Green;
-            this.label6.Location = new System.Drawing.Point(266, 107);
+            this.label6.Location = new System.Drawing.Point(266, 120);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(166, 21);
             this.label6.TabIndex = 8;
             this.label6.Text = "Tip: you can use Win+.";
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 21);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Current face:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(270, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 37);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Launch Game!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 505);
+            this.ClientSize = new System.Drawing.Size(453, 511);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -252,6 +275,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CWFC by Rider";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -268,6 +292,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CWCF/Form1.cs
+++ CWCF/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Diagnostics;
 using System.Text;
 using System.Windows.Forms;
 
@@ -27,6 +28,7 @@ namespace CWCF
                 key.SetValue("FaceText_h3883740665", utf8Bytes, RegistryValueKind.Binary);
                 key.Close();
                 MessageBox.Show("The face has been successfully set!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ReadValueFromRegistry();
             }
             catch (Exception ex)
             {
@@ -56,6 +58,37 @@ namespace CWCF
                 textBox1.Clear();
                 textBox1.Text = listBox1.SelectedItem.ToString();
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ReadValueFromRegistry();
+        }
+
+
+        private void ReadValueFromRegistry()
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Landfall Games\Content Warning");
+                if (key != null)
+                {
+                    byte[] utf8Bytes = (byte[])key.GetValue("FaceText_h3883740665", new byte[0]);
+                    string text = Encoding.UTF8.GetString(utf8Bytes);
+                    label7.Text ="Current face: "+ text;
+                    key.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("steam://rungameid/2881650");
         }
     }
 }
