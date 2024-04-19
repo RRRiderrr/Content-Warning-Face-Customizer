e5dc76217453786a04da3e7dfecbfd12db897633
RRRiderrr <77801822+RRRiderrr@users.noreply.github.com>
2024-04-19 17:32:47 +0300

    Add files via upload

---
+++ CWCF/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace CWCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(172, 428);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Set face!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(148, 104);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Items.AddRange(new object[] {
+            "ㅇㅅㅇ",
+            "•ㅅ•",
+            "•ᴥ•",
+            "ᵔᴥᵔ",
+            "Ò益Ó",
+            "ÒwÓ",
+            "♡ω♡",
+            "^﹏^",
+            "・∀・",
+            "─~─",
+            "￢ ヘ￢",
+            "☐_☐",
+            "￢ヮ￢",
+            "`ʖ´",
+            "ºωº",
+            "￣3￣",
+            "◉_◉",
+            "◉ᴗ◉",
+            "◎﹃◎",
+            "◎_◎",
+            "⊙▃⊙",
+            "⊙д⊙",
+            "⊙︿⊙",
+            "♀⌒♀",
+            "●ᴥ●",
+            "≧ヮ≦",
+            "•ω•",
+            "°д°",
+            "óωò",
+            "Ŏ˕Ŏ",
+            "ŎɷŎ",
+            "ϾϖϿ",
+            "⓿_⓿",
+            "◕‿◕",
+            "ΦᆺΦ",
+            "◉﹏◉",
+            "☐﹏☐",
+            "◑﹏◐",
+            ">∩<",
+            "^☐^",
+            "☐~☐",
+            "￣o￣",
+            "•_•",
+            "-ω-",
+            "•☐•",
+            "ȍ☐ȍ",
+            "◓_◓",
+            "Ħ_Ħ",
+            "罒⌒罒",
+            "罓⌒罓",
+            "马⌒马",
+            "(ㅅ)",
+            "☯",
+            "♡",
+            "♥",
+            "♦",
+            "☆",
+            "★",
+            "◉",
+            "▣",
+            "▬",
+            "☖",
+            "Ϙ",
+            "Θ",
+            "Δ",
+            "θ",
+            "Π",
+            "⚠",
+            "卐",
+            "♠",
+            "☎️",
+            "░",
+            "▒",
+            "▓",
+            "▣",
+            "☺",
+            "☻",
+            "🐟",
+            "😊",
+            "😋",
+            "😍",
+            "😎",
+            "😀",
+            "😁",
+            "😂",
+            "😃",
+            "😅",
+            "😆",
+            "😉",
+            "☀",
+            "☁",
+            "☂",
+            "☃",
+            "☉"});
+            this.listBox1.Location = new System.Drawing.Point(232, 158);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(126, 235);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 107);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Set custom face:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 158);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Or choose one from presets:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 475);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 21);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Made by Rider v1.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(3, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(449, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Content Warning Face Customizer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(25, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(407, 21);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Note: Make sure you closed the game before setting face!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Green;
+            this.label6.Location = new System.Drawing.Point(266, 107);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(166, 21);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tip: you can use Win+.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 505);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CWFC by Rider";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CWCF/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CWCF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string inputText = textBox1.Text;
+
+            
+            byte[] utf8Bytes = Encoding.UTF8.GetBytes(inputText);
+
+            
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Landfall Games\Content Warning");
+                key.SetValue("FaceText_h3883740665", utf8Bytes, RegistryValueKind.Binary);
+                key.Close();
+                MessageBox.Show("The face has been successfully set!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+            {
+                button1.Enabled = false;
+            }
+            else
+                button1.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                textBox1.Clear();
+                textBox1.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+    }
+}
